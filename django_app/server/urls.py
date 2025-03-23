111b09be924f7b4b6fe9b2f12a090f614248f419
Hugo Moraes <hugomoraes013579@gmail.com>
2025-03-23 19:36:48 -0400

    Add QuizViewSet and modify other viewsets, models and serializers

--- django_app/server/urls.py
+++ django_app/server/urls.py
@@ -1,7 +1,10 @@
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet
+from .views import UserViewSet, QuizViewSet, ClassroomViewSet, ProfessorViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
+router.register(r'quiz', QuizViewSet, basename='quiz')
+router.register(r'classroom', ClassroomViewSet, basename='classroom')
+router.register(r'professor', ProfessorViewSet, basename='professor')
 
 urlpatterns = router.urls
