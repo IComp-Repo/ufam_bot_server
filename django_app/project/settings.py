1a7bf605a4847cc49540458812c672d275a21bfc
HugMoraes <hugomoraes013579@gmail.com>
2025-06-16 08:01:48 -0400

    fix(django) : Fix deploy error on change

--- django_app/project/settings.py
+++ django_app/project/settings.py
@@ -57,10 +57,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CSRF_TRUSTED_ORIGINS = [
-    'https://bot-telegram-test-server1.onrender.com'
-]
-
 CORS_ALLOWED_ORIGINS = [
     'bot-telegram-test-server1.onrender.com',
     "https://poll-miniapp.vercel.app",
