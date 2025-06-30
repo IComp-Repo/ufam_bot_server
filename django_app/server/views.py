289e15dfec8b8b51d6b87876676434f47f62e91e
sDowlani <sahilvdowlani@outlook.com>
2025-06-29 23:45:08 -0400

    fix/ calling wrong render endpoint

--- django_app/server/views.py
+++ django_app/server/views.py
@@ -76,7 +76,7 @@ class TelegramWebhookView(APIView):
 
             chat_title = chat.get("title")
 
-            bind_response = requests.post(f'https://bot-telegram-test-server1.onrender.com/swagger/api/bind-group/', json={
+            bind_response = requests.post(f'https://bot-telegram-test-server1.onrender.com/api/bind-group/', json={
                 'telegram_id': sender_id,
                 'chat_id': chat_id,
                 'chat_title': chat_title
